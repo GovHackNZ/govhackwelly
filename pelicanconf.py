d039aba27e0294432f700070dc0a7f9a06725503
Lenz Gschwendtner <lenz@springtimesoft.com>
2015-05-08 11:28:41 +1200

    added a site root

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'aimee whitcroft'
 SITENAME = u'GovHack Wellington'
-SITEURL = ''
+SITEURL = 'https://govhacknz.github.io/govhackwelly/'
 
 PATH = 'content'
 
